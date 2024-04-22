f5bd56e727269e14b89ea87b4597bb09951614ef
Wagner <wagner_silvajr@hotmail.com>
2024-04-22 16:02:20 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
